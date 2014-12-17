c0c601f811bcb887712b64c78b2d5d39b63b1dd4
srph <seraphipod@gmail.com>
2014-12-18 02:09:38 +0800

    Removed manual Date.parse test

--- tests/filter.spec.js
+++ tests/filter.spec.js
@@ -1,13 +1,10 @@
 describe('filter', function() {
   beforeEach(module('srph.timestamp-filter'));
 
-  it('should parse input to timestamp', function() {
+  it('should be equal to Date.parse of the same date', function() {
     inject(function(timestampFilter) {
       expect(timestampFilter('11/23/1996')).toEqual(Date.parse('11/23/1996'));
       expect(timestampFilter('12-25-2014 12:00:00')).toEqual(Date.parse('12-25-2014 12:00:00'));
-
-      // Manual
-      expect(timestampFilter('11/23/1996 00:00:00')).toEqual(848678400000);
     });
   });
 });

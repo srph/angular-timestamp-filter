73630a7b03cf1089bef06eaf3c247874273f7b55
srph <seraphipod@gmail.com>
2015-03-10 23:35:04 +0800

    Added Firefox to browsers to test against by KarmaJS.

--- karma.conf.js
+++ karma.conf.js
@@ -58,23 +58,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS', 'PhantomJS_custom'],
-
-    // phantomjs
-    // https://github.com/karma-runner/karma-phantomjs-launcher
-    customLaunchers: {
-      'PhantomJS_custom': {
-        base: 'PhantomJS',
-        options: {
-          windowName: 'my-window',
-          settings: {
-            webSecurityEnabled: false
-          }
-        },
-        flags: ['--remote-debugger-port=9000']
-      }
-    },
-
+    browsers: ['PhantomJS', 'Firefox'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits

f82305bd38c6d7b78d89687670e743f5c038366c
Kier Borromeo <seraphipod@gmail.com>
2015-03-10 22:30:53 +0800

    Refactored test

--- tests/filter.spec.js
+++ tests/filter.spec.js
@@ -1,10 +1,13 @@
 describe('filter', function() {
   beforeEach(module('srph.timestamp-filter'));
+  
+  var filter;
+  beforeEach(inject(function(timestampFilter) {
+      filter = timestampFilter;
+  }));
 
   it('should be equal to Date.parse of the same date', function() {
-    inject(function(timestampFilter) {
-      expect(timestampFilter('11/23/1996')).toEqual(Date.parse('11/23/1996'));
-      expect(timestampFilter('12-25-2014 12:00:00')).toEqual(Date.parse('12-25-2014 12:00:00'));
-    });
+    expect(timestampFilter('11/23/1996')).toEqual(Date.parse('11/23/1996'));
+    expect(timestampFilter('12-25-2014 12:00:00')).toEqual(Date.parse('12-25-2014 12:00:00'));
   });
 });

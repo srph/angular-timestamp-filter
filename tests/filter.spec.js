fd85025ea11e7c9ebce0b977313b5a7e3c86d75b
Kier Borromeo <seraphipod@gmail.com>
2015-03-10 22:31:16 +0800

    Fixed wrong call

--- tests/filter.spec.js
+++ tests/filter.spec.js
@@ -7,7 +7,7 @@ describe('filter', function() {
   }));
 
   it('should be equal to Date.parse of the same date', function() {
-    expect(timestampFilter('11/23/1996')).toEqual(Date.parse('11/23/1996'));
-    expect(timestampFilter('12-25-2014 12:00:00')).toEqual(Date.parse('12-25-2014 12:00:00'));
+    expect(filter('11/23/1996')).toEqual(Date.parse('11/23/1996'));
+    expect(filter('12-25-2014 12:00:00')).toEqual(Date.parse('12-25-2014 12:00:00'));
   });
 });

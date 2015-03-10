14d1b1e19dde4e60fbceff2060e42bf9d1845c93
Kier Borromeo <seraphipod@gmail.com>
2015-03-10 22:39:42 +0800

    Update filter.spec.js

--- tests/filter.spec.js
+++ tests/filter.spec.js
@@ -7,7 +7,11 @@ describe('filter', function() {
   }));
 
   it('should be equal to Date.parse of the same date', function() {
-    expect(filter('11/23/1996')).toEqual(Date.parse('11/23/1996'));
     expect(filter('12-25-2014 12:00:00')).toEqual(Date.parse('12-25-2014 12:00:00'));
+  });
+  
+  it('should convert dashes to slashes to fix incompatibility issue', function() {
+    spyOn('', 'replace');
+    expect(filter('11/23/1996')).toEqual(Date.parse('11-23-1996'));
   });
 });

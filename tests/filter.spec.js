3fe377883fc6a5822d2e2fa43bb548adcbaeeab2
srph <seraphipod@gmail.com>
2015-03-10 23:30:37 +0800

    Updated tests.
    Added test for the regex.
    Added test for the string replace implementation.
    
    Updated bower's config; moved `angular` as `dependency` instead of `devDependency`.

--- tests/filter.spec.js
+++ tests/filter.spec.js
@@ -7,15 +7,43 @@ describe('filter', function() {
   }));
 
   it('should be equal to Date.parse of the same date', function() {
-    expect(filter('12-25-2014 12:00:00')).toEqual(Date.parse('12-25-2014 12:00:00'));
+    expect(filter('12/25/2014 12:00:00')).toEqual(Date.parse('12/25/2014 12:00:00'));
   });
-  
-  // 11-23-1996 to 11/23/1996
-  it('should convert dashes to slashes when a string is passed to fix incompatibility issue', function() {
-    var date = '11-23-1996';
-    
-    spyOn(date, 'replace').and.returnValue('11/23/1996');
-    expect(filter('11/23/1996')).toEqual(Date.parse(date));
-    expect(date.replace).toHaveBeenCalled();
+
+  describe('dash to slash conversion', function() {
+    // To test the regex used in the filter
+    describe('dash regex', function() {
+      // Let's first test if it matches strings with dashes
+      it('should match dashes', function() {
+        expect(/\-/g.test('-\\1zxcqw-/zxc12346--8-O')).toBe(true);
+      });
+
+      it('should not match string with no dashes', function() {
+        expect(/\-/g.test('asdlkajlskdasd/asda\\')).toBe(false);
+      })
+      
+      // Let's test if our regex actually converts all dashes to slashes
+      it('should convert all dash to slash', function() {
+        expect('------'.replace(/\-/g, '/')).toEqual('//////');
+      });
+
+      it('should convert all dash to kier-pogi', function() {
+        expect('------'.replace(/\-/g, 'kier-pogi')).toEqual('kier-pogikier-pogikier-pogikier-pogikier-pogikier-pogi');
+      });
+    });
+
+    it('should occur when a string is passed to fix incompatibility issue', function() {
+      var date = '11-23-1996';
+      
+      spyOn(String.prototype, 'replace').and.returnValue('11/23/1996');
+      expect(filter(date)).toEqual(Date.parse('11/23/1996'));
+      expect(String.prototype.replace).toHaveBeenCalled();
+    });
+
+    it('should not occur when the provided date is not a string', function() {
+      spyOn(String.prototype, 'replace');
+      filter(new Date());
+      expect(String.prototype.replace).not.toHaveBeenCalled();
+    });
   });
 });
